c0cca8a8759874c127c8fd64e4838da267670300
Ziyi Huang <ihuangziyi@gmail.com>
2026-07-16 15:23:40 +0800

    feat: expose dataset and model API

--- src/voc_easyensemble/__init__.py
+++ src/voc_easyensemble/__init__.py
@@ -1,3 +1,13 @@
-"""VOC EasyEnsemble package."""
+"""Reliable small-sample VOC classification pipeline."""
+
+from .data import VOCDataset, load_voc_mat
+from .model import EasyEnsembleConfig, VOCEasyEnsemble
+
+__all__ = [
+    "VOCDataset",
+    "load_voc_mat",
+    "EasyEnsembleConfig",
+    "VOCEasyEnsemble",
+]
 
 __version__ = "1.0.0"
